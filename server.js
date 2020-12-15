babec49351e2fdcd8ab22e2e146a17648f77ea21
dcameronnguyen <dcameron.nguyen@gmail.com>
2020-12-14 19:36:42 -0500

    removed redundant code

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
+require("dotenv").config();
 
-require('dotenv').config();
+const usersRouter = require("./routes/api/users");
 
-const scoresRouter = require('./routes/api/scores');
-const usersRouter = require('./routes/api/users');
+require("./config/database");
 
-
-require('./config/database');
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
 
-
-app.use('/api/scores', scoresRouter);
-app.use('/api/users', usersRouter);
+app.use("/api/users", usersRouter);
 
 const port = process.env.PORT || 3001;
-	
-app.listen(port, function() {
-  console.log(`Express app running on port ${port}`)
+
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
 });
